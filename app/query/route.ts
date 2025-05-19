1ea7e05785b6d14aef80c135f8000a9716896d13
Tetiana Belik <tetiana.an.belik@gmail.com>
2025-05-19 16:07:10 +0200

    force deploy

--- app/query/route.ts
+++ app/query/route.ts
@@ -16,7 +16,7 @@
 export async function GET() {
   return Response.json({
     message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      'Uncomment this file and remove this line. You can delete this file when you are finished.1',
   });
   // try {
   // 	return Response.json(await listInvoices());
